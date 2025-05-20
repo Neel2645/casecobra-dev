62e8ac762c8a14d0649eda588f7fa7a9c3827343
use <sathvaraneel52@gmail.com>
2025-05-21 02:41:39 +0530

    steps in configure mostly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        {children}
-        <Footer/>
+         <Navbar/>
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>
+           {children}
+          </div>
+          <Footer />
+        </main>
+        <Toaster/>
       </body>
     </html>
   );
