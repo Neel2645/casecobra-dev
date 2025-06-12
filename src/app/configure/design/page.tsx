f52b61f62941b4745f9683aeff7c9a8163ea8b9e
use <sathvaraneel52@gmail.com>
2025-06-13 03:50:53 +0530

    some

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,55 +1,18 @@
-// "use server"
-
-// import { db } from "@/db";
-// import { notFound } from "next/navigation";
-// import DesignConfigurator from "./DesignConfigurator";
-
-// interface PageProps {
-//   searchParams: {
-//     [key: string]: string | string[] | undefined;
-//   };
-// }
-
-// const Page = async ({ searchParams }: PageProps) => {
-//   const { id } = searchParams;
-
-
-//   if (!id || typeof id !== "string") {
-//     return notFound();
-//   }
-
-//   const configuration = await db.configuration.findUnique({
-//     where: { id },
-//   });
-
-//   if (!configuration) {
-//     return notFound();
-//   }
-
-//   const { imageUrl, width, height } = configuration;
-
-//   return (
-//     <DesignConfigurator
-//       configId={configuration.id}
-//       imageDimensions={{ width, height }}
-//       imageUrl={imageUrl}
-//     />
-//   );
-// };
-
-// export default Page;
-
+"use server"
 
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import DesignConfigurator from "./DesignConfigurator";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  const id = searchParams?.id;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
 
   if (!id || typeof id !== "string") {
     return notFound();
@@ -72,4 +35,9 @@ export default async function Page({
       imageUrl={imageUrl}
     />
   );
-}
+};
+
+export default Page;
+
+
+
